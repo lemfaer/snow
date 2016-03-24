9700ff3fdbc60830ecdb64648a7bc5de7df0b29b
lem <lemfaer@gmail.com>
2016-03-24 21:50:53 +0200

    template view update
    head, header, footer added

---
+++ views/template/header.php
@@ -0,0 +1,71 @@
+<!-- REQUIRE -->
+	<!-- $cart -->
+<!-- REQUIRE END -->
+
+<body>
+
+<div class="header">
+	<div class="container">
+		<div class="row">
+			<div class="header-left">
+				<div class="logo">
+					<a href="index.html"><img src="/template/images/logo.png" alt=""/></a>
+				</div>
+				<div class="menu">
+					<a class="toggleMenu" href="#">
+						<img src="/template/images/nav.png" alt="" />
+					</a>
+					<ul class="nav" id="nav">
+						<li><a href="/category">Shop</a></li>
+						<li><a href="team.html">Team</a></li>
+						<li><a href="shop.html">Events</a></li>
+						<li><a href="experiance.html">Experiance</a></li>
+						<li><a href="shop.html">Company</a></li>
+						<li><a href="contact.html">Contact</a></li>
+						<div class="clear"></div>
+					</ul>
+						
+				</div>							
+				<div class="clear"></div>
+			</div>
+			<div class="header_right">
+				<ul class="icon1 sub-icon1 profile_img">
+					<li>
+						<a class="active-icon c1" href="#"> </a>
+						<ul class="sub-icon1 list">
+								
+							<div id="mini-cart"></div>
+								
+							<div class="clear"></div>
+							<div class="login_buttons">
+								<div class="check_button">
+									<a href="checkout.html">Check out</a>
+								</div>
+								<div class="clear"></div>
+							</div>
+							<div class="clear"></div>
+						</ul>
+					</li>
+				</ul>
+
+				<ul class="icon1 sub-icon1 profile_img">
+					<li>
+						<a class="active-icon c1" href="#"> </a>
+						<ul class="sub-icon1 list">
+							<div class="clear"></div>
+							<div class="login_buttons">
+								<div class="check_button">
+									<a href="checkout.html">Check out</a>
+								</div>
+								<div class="clear"></div>
+							</div>
+							<div class="clear"></div>
+						</ul>
+					</li>
+				</ul>
+
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+</div>
