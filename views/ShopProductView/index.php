e4a329456fd3fe0d9ca335f28620bdce089899d1
lem <lemfaer@gmail.com>
2016-03-24 21:56:59 +0200

    product view update
    code usability increase

--- views/ShopProductView/index.php
+++ views/ShopProductView/index.php
@@ -1,22 +1,38 @@
-<script type="text/javascript" src="/views/ShopProductView/product-options.js"></script>
 
-<?php 
-	$product = $productItem->getProduct();
-?>
+<!-- REQUIRED -->
+	<!-- $productItem    -->
+	<!-- $recomendedList -->
+<!-- REQUIRED END -->
+
+<?php $product   = $productItem->getProduct(); ?>
+
+<?php $charList  = $productItem->getCharList(); ?>
+<?php $sizeList  = $productItem->getSizeList(); ?>
+<?php $colorList = $productItem->getColorList(); ?>
+<?php $imageList = $productItem->getImageList(); ?>
+
+<?php $pAvailable = $product->isAvailable(); ?>
+<?php $pPrice     = "$".$product->getPrice(); ?>
+<?php $pDesc      = $product->getDescription(); ?>
+<?php $pShortDesc = $product->getShortDescription(); ?>
+<?php $pName      = $product->getProducer()->getName()
+	.' '.$product->getName().' '.$product->getYear(); ?>
 
 <div class="main">
 	<div class="shop_top">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-9 single_left">
+				<div class="col-lg-9 col-md-9 col-sm-8 col-xs-8 single_left">
 					<div class="single_image">
 						<div class="flexslider">
 			  				<ul class="slides">
 								
-								<?php foreach ($productItem->getImageList() as $value): ?>
-									<li data-thumb="<?php echo $value->link(); ?>">
+								<?php foreach ($imageList as $image): ?>
+									<?php $link = $image->link(); ?>
+
+									<li data-thumb="<?= $link; ?>">
 										<div class="thumb-image"> 
-											<img src="<?php echo $value->link(); ?>" 
+											<img src="<?= $link; ?>" 
 												data-imagezoom="true" class="img-responsive"> 
 										</div>
 									</li>
@@ -26,32 +42,30 @@
 						</div>
 					</div>
 					<div class="single_right">
-						<h3>
-							<?php echo $product->getProducer()->getName()
-								.' '.$product->getName()
-								.' '.$product->getYear(); ?>
-						</h3>
-						<p class="m_10"><?php echo $product->getShortDescription(); ?></p>
+						<h3><?= $pName; ?></h3>
+						<p class="m_10"><?= $pShortDesc; ?></p>
 				        	
-				        	<?php if ($product->isAvailable()): ?>
+				        	<?php if ($pAvailable): ?>
 
-					        	<?php if (count($productItem->getSizeList()) > 1): ?>
+					        	<?php if (count($sizeList) > 1): ?>
 									<ul class="options">
 										<h4 class="m_12">Select a Size(cm)</h4>
-										<?php foreach ($productItem->getSizeList() as $value): ?>
-											<li><a><?php echo $value['size']->getName(); ?></a></li>
+										<?php foreach ($sizeList as $value): ?>
+											<?php $name = $value['size']->getName(); ?>
+
+											<li><a><?= $name; ?></a></li>
 										<?php endforeach; ?>
 									</ul>
 								<?php endif; ?>
 
-								<?php if (count($productItem->getColorList()) > 1): ?>
+								<?php if (count($colorList) > 1): ?>
 									<ul class="product-colors">
 										<h3>available Colors</h3>
-										<?php foreach ($productItem->getColorList() as $value): ?>
+										<?php foreach ($colorList as $value): ?>
+											<?php $value = $value['color']->getValue(); ?>
+
 											<li><a>
-												<span style="background: 
-													<?php echo $value['color']->getValue(); ?>
-												"></span>
+												<span style="background: <?= $value; ?>"></span>
 											</a></li>
 										<?php endforeach; ?>
 										<div class="clear"> </div>
@@ -68,16 +82,20 @@
 	 			            </ul>
 
 							<div class="social_buttons">
-								<button type="button" class="btn1 btn1-default1 btn1-twitter" onclick="">
+								<button type="button" 
+									class="btn1 btn1-default1 btn1-twitter" onclick="">
 					            	<i class="icon-twitter"></i> Tweet
 					            </button>
-					            <button type="button" class="btn1 btn1-default1 btn1-facebook" onclick="">
+					            <button type="button" 
+					            	class="btn1 btn1-default1 btn1-facebook" onclick="">
 					            	<i class="icon-facebook"></i> Share
 					            </button>
-					             <button type="button" class="btn1 btn1-default1 btn1-google" onclick="">
+					             <button type="button" 
+					             	class="btn1 btn1-default1 btn1-google" onclick="">
 					            	<i class="icon-google"></i> Google+
 					            </button>
-					            <button type="button" class="btn1 btn1-default1 btn1-pinterest" onclick="">
+					            <button type="button" 
+					            	class="btn1 btn1-default1 btn1-pinterest" onclick="">
 					            	<i class="icon-pinterest"></i> Pinterest
 					            </button>
 					        </div>
@@ -85,93 +103,37 @@
 				        <div class="clear"> </div>
 					</div>
 
-				<?php if ($product->isAvailable()): ?>	
-					<div class="col-md-3">
+				<?php if ($pAvailable): ?>	
+					<div class="col-lg-3 col-md-3 col-sm-4 col-xs-4">
 						<div class="box-info-product">
-							<p class="price2">$<?php echo $product->getPrice(); ?></p>
+							<p class="price2"><?= $pPrice; ?></p>
 
-							<form class="product-options">
-								
-								<?php if (count($productItem->getColorList()) > 1): ?>
-									<ul class="op-group" op-name="color">
-
-										<?php foreach ($productItem->getColorList() as $value): ?>
-											<?php $value['size_id'] = implode(",", $value['size_id']); ?>
-											<li class="op-li">
-												<input type="radio" 
-													class="op-radio" 
-													name="op-color" 
-													id="op-color-<?php echo $value['color']->getID(); ?>"
-													value="<?php echo $value['color']->getID(); ?>"
-													fgn-name="size"
-													fgn-id="<?php echo $value['size_id']; ?>">
-												<label class="op-label" 
-													for="op-color-<?php echo $value['color']->getID(); ?>">
-													<span class="op-color"
-														style="background: 
-															<?php echo $value['color']->getValue(); ?>">
-													</span>
-												</label>
-											</li>
-										<?php endforeach; ?>
-
-									</ul>								
-								<?php endif; ?>
-								
-								<?php if (count($productItem->getSizeList()) > 1): ?>
-									<ul class="op-group" op-name="size">
-
-										<?php foreach ($productItem->getSizeList() as $value): ?>
-											<?php $value['color_id'] = implode(",", $value['color_id']); ?>
-											<li class="op-li">
-												<input type="radio" 
-													class="op-radio" 
-													name="op-size" 
-													id="op-size-<?php echo $value['size']->getID(); ?>"
-													value="<?php echo $value['size']->getID(); ?>"
-													fgn-name="color"
-													fgn-id="<?php echo $value['color_id']; ?>">
-												<label class="op-label" 
-													for="op-size-<?php echo $value['size']->getID(); ?>">
-													<span class="op-text">
-														<?php echo $value['size']->getName(); ?>
-													</span>
-												</label>
-											</li>
-										<?php endforeach ?>
-
-									</ul>
-								<?php endif; ?>
-
-								<button type="submit" name="Submit" class="exclusive">
-									<span>Add to cart</span>
-								</button>
-							</form>
+							<?php require_once(ROOT."/views/ShopProductView/product-options.php"); ?>
 						</div>
 					</div>
 				<?php endif; ?>
 				
 			</div>
 
-			<?php if ($product->getDescription()): ?>
+			<?php if ($pDesc): ?>
 				<div class="desc">
 					<h4>Description</h4>
-					<p><?php echo $product->getDescription(); ?></p>
+					<p><?= $pDesc; ?></p>
 				</div>				
 			<?php endif ?>
 
-			<?php if ($productItem->getCharList()): ?>
+			<?php if ($charList): ?>
 				<div class="product-specifications">
 					<h4>Characteristics</h4>
 					<ul>
-						<?php foreach ($productItem->getCharList() as $value): ?>
+						<?php foreach ($charList as $charItem): ?>
+							<?php $cName  = $charItem->getName(); ?>
+							<?php $cValue = $charItem->getValue(); ?>
+							<?php $name   = $cName->getName(); ?>
+							<?php $value  = $cValue->getValue(); ?>
 							<li>
-								<span class="specification-heading">
-									<?php echo $value->getName()->getName(); ?>
-								</span>
-								<span>
-									<?php echo $value->getValue()->getValue(); ?>
-								</span>
+								<span class="specification-heading"><?= $name; ?></span>
+								<span><?= $value; ?></span>
 								<div class="clear"></div>
 							</li>
 						<?php endforeach; ?>
@@ -182,25 +144,26 @@
 			<div class="row">
 				<h4 class="m_11">Related Products //in the same Category</h4>
 				
-				<?php foreach ($recomendedList as $value): ?>
-					<div class="col-md-4 product1">
-						<img src="<?php echo $value->getImage()->link(); ?>" class="img-responsive" alt=""/> 
+				<?php foreach ($recomendedList as $rec): ?>
+					<?php $price     = '$'.$rec->getPrice(); ?>
+					<?php $image     = $rec->getImage()->link(); ?>
+					<?php $link      = "/product/".$rec->getID(); ?>
+					<?php $shortDesc = $rec->getShortDescription(); ?>
+					<?php $name      = $rec->getProducer()->getName()
+						.' '.$rec->getName().' '.$rec->getYear(); ?>
+
+					<div class="col-lg-3 col-md-3 col-sm-4 col-xs-6 product1">
+						<img src="<?= $image; ?>" class="img-responsive" alt=""/> 
 						<div class="shop_desc">
-							<a href="/product/<?php echo $value->getID(); ?>"></a>
+							<a href="<?= $link; ?>"></a>
 							<h3>
-								<a href="/product/<?php echo $value->getID(); ?>"></a>
-								<a href="/product/<?php echo $value->getID(); ?>">
-									<?php echo $value->getProducer()->getName()
-									.' '.$value->getName()
-									.' '.$value->getYear(); ?>
-								</a>
+								<a href="<?= $link; ?>"></a>
+								<a href="<?= $link; ?>"><?= $name; ?></a>
 							</h3>
-							<p><?php echo $value->getShortDescription(); ?></p>
-							<span class="actual">$<?php echo $value->getPrice(); ?></span><br>
+							<p><?= $shortDesc; ?></p>
+							<span class="actual"><?= $price; ?></span><br>
 							<ul class="buttons">
-								<li class="shop_btn">
-									<a href="/product/<?php echo $value->getID(); ?>">Read More</a>
-								</li>
+								<li class="shop_btn"><a href="<?= $link; ?>">Read More</a></li>
 								<div class="clear"> </div>
 							</ul>
 						</div>
