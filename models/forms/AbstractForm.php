fafe0ead3d14f63e85b4048c9072dedc835c83e4
lem <lemfaer@gmail.com>
2016-03-30 13:51:02 +0300

    AbstractForm documentation added

--- models/forms/AbstractForm.php
+++ models/forms/AbstractForm.php
@@ -1,7 +1,27 @@
 <?php
 
+/**
+ * Описывает структуру классов выполняющих проверку форм
+ * и выполняющих необходимые действия после отправки.
+ * 
+ * @package models_forms
+ * @author  Alan Smithee
+ * @abstract
+ */
 abstract class AbstractForm {
 
+	/**
+	 * Изначальная реализация метода check
+	 * Выполняет валидацию данных полученных из формы
+	 * 
+	 * Выполняет валидацию данных из формы на основе нужного валидатора 
+	 * и метода валидации. Формирует массив для отправки обратно на форму
+	 * 
+	 * @param array $data массив переданный из формы
+	 * @param Closure $valMethod замыкание возвращает метод для валидации полученных данных
+	 * @param AbstractValidator $validator валидатор
+	 * @return type
+	 */
 	protected static function checkParamsDefault(array $data, Closure $valMethod, AbstractValidator $validator) : array {
 		$check = true;
 		foreach ($data as $key => $value) {
@@ -17,8 +37,20 @@ abstract class AbstractForm {
 		return $result;
 	}
 
-	abstract static function check(array $data) : string;
+	/**
+	 * Выполняет проверку данных полученных из формы
+	 * 
+	 * @param array $data массив переданный из формы
+	 * @return string массив в формате json, ответ на ajax запрос
+	 */
+	abstract public static function check(array $data) : string;
 
-	abstract static function submit(array $data);
+	/**
+	 * Выполняет необходимые действия с данными
+	 * 
+	 * @param array $data массив переданный из формы
+	 * @return void
+	 */
+	abstract public static function submit(array $data);
 
 }
