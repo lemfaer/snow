a2607747727529bdca02a676ed9212bfc68f7c82
lem <lemfaer@gmail.com>
2016-04-02 19:39:06 +0300

    names fixed
    in category selects script

--- views/admin/inherits.js
+++ views/admin/inherits.js
@@ -1,10 +1,13 @@
 jQuery(document).ready(function($) {
 
-	$("#ad-size-parent").select2({"placeholder": "Выбирите категорию"});
-	$("#ad-size-category").select2({"placeholder": "Выбирите подкатегорию"});
+	var name     = crup.name; // object crup from /views/admin/crup.js
+	var parent   = $("#ad-" + name + "-parent");
+	var category = $("#ad-" + name + "-category");
+
+	$(parent).select2({"placeholder": "Выбирите категорию"});
+	$(category).select2({"placeholder": "Выбирите подкатегорию"});
 
 	$.post("/admin/select/inherits", {"parent_id": 0}, function(data) {
-		var parent = $("#ad-size-parent");
 		$(parent).select2({
 			placeholder: "Выбирите категорию",
 			data: data,
@@ -16,25 +19,24 @@ jQuery(document).ready(function($) {
 		}
 	}, "json");
 	
-	$("#ad-size-parent").on("select2:select", function(event) {
+	$(parent).on("select2:select", function(event) {
 		var id = $(this).select2("val");
 
 		$.post("/admin/select/inherits", {"parent_id": id}, function(data) {
-			$("#ad-size-category").html('');
+			$(category).html('');
 			if(data) {
-				var category = $("#ad-size-category");
 				$(category).select2({
 					placeholder: "Выбирите подкатегорию",
 					data: data,
 				});
-				$("#ad-size-category").select2("val", "");
+				$(category).select2("val", "");
 				if($(category).hasClass("update")) {
 					var id = $(category).attr("data-id");
 					$(category).select2("val", id);
 				}
 			}
-			$("#ad-size-category").parents(".form-group").removeClass("has-error");
-			$("#ad-size-category").parents(".form-group").removeClass("has-success");
+			$(category).parents(".form-group").removeClass("has-error");
+			$(category).parents(".form-group").removeClass("has-success");
 		}, "json");
 	});
 
