078a103039322b73f3774a7c9327b28281707cab
lem <lemfaer@gmail.com>
2016-04-05 16:41:08 +0300

    checkimage added
    in ImageValidator

--- models/tables/validators/ImageValidator.php
+++ models/tables/validators/ImageValidator.php
@@ -5,7 +5,12 @@ class ImageValidator extends AbstractTableValidator {
 //const
 	const CLASS_NAME = "Image";
 
+	const IMAGE_MIN_X = 700;
+	const IMAGE_MIN_Y = 700;
+
 	const PATH_ERROR = "Указан неправильный путь";
+
+	const IMAGE_MIN_XY_ERROR   = "Размер изображения меньше 700x700";
 //const end
 
 //validate methods
@@ -14,6 +19,13 @@ class ImageValidator extends AbstractTableValidator {
 		$path = ROOT."/images/".$path;
 		//return parent::log(file_exists($path), $error);
 		return true;
+	}
+
+	private function checkImage($im) : bool {
+		$error = array("image" => self::IMAGE_MIN_XY_ERROR);
+		$bool  = (imagesx($im) >= self::IMAGE_MIN_X)
+			&& (imagesy($im) >= self::IMAGE_MIN_Y);
+		return  parent::log($bool, $error);
 	}
 //validate methods end
 
