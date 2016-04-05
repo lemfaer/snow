9c93d20ed4c8ca8ee58f98c3a9633591559009ab
lem <lemfaer@gmail.com>
2016-04-05 16:38:19 +0300

    path50, path135 added

--- models/tables/Image.php
+++ models/tables/Image.php
@@ -6,25 +6,45 @@ class Image extends AbstractTable {
 
 //main info
 	//protected $id
-	private $path;
+	private $path700;
+	private $path135;
+	private $path50;
 	//protected $status
 
 	//getters
 	public function getPath() : string {
-		return parent::get($this->path);
+		return parent::get($this->path700);
+	}
+
+	public function getPath135() : string {
+		return parent::get($this->path135);
+	}
+
+	public function getPath50() : string {
+		return parent::get($this->path50);
 	}
 	//getters end
 
 	//setters
 	public function setPath(string $path) {
-		$this->path = parent::set($path, "checkPath");
+		$this->path700 = parent::set($path, "checkPath");
+	}
+
+	public function setPath135(string $path135) {
+		$this->path135 = parent::set($path135, "checkPath");
+	}
+
+	public function setPath50(string $path50) {
+		$this->path50 = parent::set($path50, "checkPath");
+	}
+
 	}
 	//setters end
 //main info end
 
 //link
 	public function link() {
-		return $this->path;
+		return $this->path700;
 	}
 //link end
 
@@ -36,9 +56,11 @@ class Image extends AbstractTable {
 	protected static function withArray(array $arr) : AbstractTable {
 		$obj = new self();
 
-		$obj->id     = (int)    $arr['id'];
-		$obj->path   = (string) $arr['path'];
-		$obj->status = (bool)   $arr['status'];
+		$obj->id      = (int)    $arr['id'];
+		$obj->path700 = (string) $arr['path700'];
+		$obj->path135 = (string) $arr['path135'];
+		$obj->path50  = (string) $arr['path50'];
+		$obj->status  = (bool)   $arr['status'];
 
 		return $obj;
 	}
