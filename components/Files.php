88372cd7ef059f9f74ee02a04f1f3133b1e3d9b5
lem <lemfaer@gmail.com>
2016-04-06 21:23:51 +0300

    Files added
    class for restructuring $_FILES

---
+++ components/Files.php
@@ -0,0 +1,41 @@
+<?php
+
+class Files {
+
+	private static $_files;
+
+	public static function get() : array {
+		if(!isset(self::$_files)) {
+			self::_files();
+		}
+		return self::$_files;
+	}
+
+//construct
+	private static function _files() {
+		$files = $_FILES;
+
+		foreach ($files as $key => $value) {
+			$files[$key] = array();
+			foreach (self::rec($value) as $value) {
+				$files[$key] = array_merge_recursive($files[$key], $value);
+			}
+		}
+
+		self::$_files = $files;
+	}
+
+	private static function rec($tix, string $field = null) {
+		if(is_array($tix)) {
+			foreach ($tix as $key => $value) {
+				$tix[$key] = self::rec($value, $field ?? $key);
+			}
+		} else {
+			$tix = array($field => $tix);
+		}
+
+		return $tix;
+	}
+//construct end
+
+}
