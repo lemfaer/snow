2c37783d1b0b51e3b681cf45356ea4984c8eab23
lem <lemfaer@gmail.com>
2016-04-06 21:30:16 +0300

    image validator null check added

--- models/tables/validators/ImageValidator.php
+++ models/tables/validators/ImageValidator.php
@@ -10,8 +10,9 @@ class ImageValidator extends AbstractTableValidator {
 
 	const PATH_ERROR = "Указан неправильный путь";
 
-	const IMAGE_TYPE_ERROR = "Передан файл с неправильным расширение";
-	const IMAGE_MIN_XY_ERROR   = "Размер изображения меньше 700x700";
+	const IMAGE_NULL_ERROR   = "Изображение не передано";
+	const IMAGE_TYPE_ERROR   = "Передан файл с неправильным расширением";
+	const IMAGE_MIN_XY_ERROR = "Размер изображения меньше 700x700";
 //const end
 
 //validate methods
@@ -26,19 +27,28 @@ class ImageValidator extends AbstractTableValidator {
 		$error = array("image" => self::IMAGE_MIN_XY_ERROR);
 		$bool  = (imagesx($im) >= self::IMAGE_MIN_X)
 			&& (imagesy($im) >= self::IMAGE_MIN_Y);
-		return  parent::log($bool, $error);
+		return parent::log($bool, $error);
 	}
 
 	public function checkUploadedFile(array $uf) : bool {
-		$error = array("image" => self::IMAGE_TYPE_ERROR);
+		$bool = false;
 		$type = $uf['type'];
-		if($type !== "image/jpeg") {
-			return parent::log(false, $error);
+
+		$error = array("image" => self::IMAGE_NULL_ERROR);
+		if($type) {
+			$error = array("image" => self::IMAGE_TYPE_ERROR);
+			if($type === "image/jpeg") {
+				$bool = true;
+			}
 		}
 
-		$url = $uf['tmp_name'];
-		$im  = imagecreatefromjpeg($url); 
-		return self::checkImage($im);
+		if($bool) {
+			$url = $uf['tmp_name'];
+			$im  = imagecreatefromjpeg($url); 
+			return self::checkImage($im);
+		} else {
+			return parent::log(false, $error);
+		}
 	}
 //validate methods end
 
