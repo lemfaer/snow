70e243c69cde5e0fc67ec4b73117b561dc62305a
lem <lemfaer@gmail.com>
2016-04-10 23:47:51 +0300

    grammar fixed

--- views/admin/inherits.js
+++ views/admin/inherits.js
@@ -4,12 +4,12 @@ jQuery(document).ready(function($) {
 	var parent   = $("#ad-" + name + "-parent");
 	var category = $("#ad-" + name + "-category");
 
-	$(parent).select2({"placeholder": "Выбирите категорию"});
-	$(category).select2({"placeholder": "Выбирите подкатегорию"});
+	$(parent).select2({"placeholder": "Выберите категорию"});
+	$(category).select2({"placeholder": "Выберите подкатегорию"});
 
 	$.post("/admin/select/inherits", {"parent_id": 0}, function(data) {
 		$(parent).select2({
-			placeholder: "Выбирите категорию",
+			placeholder: "Выберите категорию",
 			data: data,
 		});
 		if($(parent).hasClass("update")) {
@@ -33,13 +33,14 @@ jQuery(document).ready(function($) {
 		$.post("/admin/select/inherits", {"parent_id": id}, function(data) {
 			if(data) {
 				$(category).select2({
-					placeholder: "Выбирите подкатегорию",
+					placeholder: "Выберите подкатегорию",
 					data: data,
 				}).select2("val", "");;
 
 				if($(category).hasClass("update")) {
 					var id = $(category).attr("data-id");
 					$(category).select2("val", id);
+					$(category).trigger("select2:select");
 				}
 			} else {
 				$(category).select2("val", "");
