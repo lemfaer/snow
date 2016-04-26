80a2bf107fab377455e3234f9fc30cffdbd6a993
lem <lemfaer@gmail.com>
2016-04-26 23:30:44 +0300

    breadcrumb updated

--- views/shop/catalog/index.php
+++ views/shop/catalog/index.php
@@ -15,31 +15,41 @@
 	});
 </script>
 
+<?php $cName = $category->getName(); ?>
+
 <div class="main">
 	<div class="shop_top">
 		<div class="container">
 
 			<div style="margin-bottom: 20px">
-				<h3 class="m_2" style="margin-bottom: 1%">
-					<?php echo $category->getName(); ?>
-				</h3>
-
-				<?php 
-					while(!($category instanceof NullCategory)) {
-						$breadcrumb[$category->link()] = $category->getName();
-						$category = $category->getParent();
-					}
-					$bcFirstName = array_shift($breadcrumb);
-				?>
+				<h3 class="m_2" style="margin-bottom: 1%"><?= $cName; ?></h3>
 
 				<ul class="breadcrumb" style="margin-bottom: 0">
+					<?php //breadcrumb
+						for($c = $category; !($c instanceof NullCategory); $c = $c->getParent()) {
+							$bc[$c->link()] = $c->getName();
+						}
+						$bc = array_reverse($bc);
+					?>
+
 					<li><a href="/category">Все категории</a></li>
-					<?php foreach($breadcrumb as $key => $value): ?>
-						<li><a href="<?php echo $key; ?>">
-							<?php echo $value; ?>
+
+					<?php foreach ($bc as $link => $crumb): ?>
+						<li><a href="<?= $link; ?>">
+							<?= $crumb; ?>
 						</a></li>
 					<?php endforeach; ?>
-					<li class="active"><?php echo $bcFirstName ?></li>
+
+					<script type="text/javascript"> // active bc
+						jQuery(document).ready(function($) {
+							var li = $(".breadcrumb li:last");
+							var a  = $(li).find("a");
+
+							$(li).addClass("active");
+							$(li).text($(a).text());
+							$(a).remove();
+						});
+					</script>
 				</ul>
 			</div>
 
