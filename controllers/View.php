e0b73e7630a031816b42628a05f16ce18d391bcf
lem <lemfaer@gmail.com>
2016-04-29 20:22:11 +0300

    template view $cart and $client added

--- controllers/View.php
+++ controllers/View.php
@@ -4,12 +4,6 @@ class View {
 
 	const HTML_COMMENT_PATTERN = "/<!--(.*?)-->/";
 
-	public static function template(string $contentPath, array $compact = array()) {
-		$code = file_get_contents(ROOT."/views/template/index.php");
-		$code = preg_replace(self::HTML_COMMENT_PATTERN, '', $code);
-		eval("?>".$code);
-	}
-
 	public static function empty(string $contentPath, array $compact = array()) {
 		$contentPath = ROOT."/views/".$contentPath;
 		$content = file_get_contents($contentPath);
@@ -19,6 +13,18 @@ class View {
 		extract($compact);
 
 		eval("?>".$content);
+	}
+
+	public static function template(string $contentPath, array $compact = array()) {
+		$code = file_get_contents(ROOT."/views/template/index.php");
+		$code = preg_replace(self::HTML_COMMENT_PATTERN, '', $code);
+
+		$cart   = Cart::get();
+		$client = Client::logged() ? Client::get() : null;
+
+		$compact = array_replace(compact("cart", "client"), $compact);
+
+		eval("?>".$code);
 	}
 
 	public static function admin(string $contentPath, array $compact = array()) {
