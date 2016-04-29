10e22b09791baecfcf2c64a780317edc9316d789
lem <lemfaer@gmail.com>
2016-04-29 20:31:48 +0300

    inherits script select use added

--- views/admin/inherits.js
+++ views/admin/inherits.js
@@ -1,51 +1,34 @@
+function updateParent(parent) {
+	// function select from select.js
+	select(parent, "/admin/select/inherits",
+		{"parent_id" : 0}, "Выберите категорию", "parent");
+}
+
+function updateCategory(category, parent) {
+	if(!$(parent).data("select2")) {
+		return;
+	}
+
+	var id = $(parent).select2("val");
+
+	// function select from select.js
+	select(category, "/admin/select/inherits", 
+		{"parent_id" : id}, "Выберите подкатегорию", "category");
+}
+
 jQuery(document).ready(function($) {
 
-	var name     = crup.name; // object crup from /views/admin/crup.js
+	var name = crup.name; // object crup from /views/admin/crup.js
+
 	var parent   = $("#ad-" + name + "-parent");
 	var category = $("#ad-" + name + "-category");
 
-	$(parent).select2({"placeholder": "Выберите категорию"});
 	$(category).select2({"placeholder": "Выберите подкатегорию"});
 
-	$.post("/admin/select/inherits", {"parent_id": 0}, function(data) {
-		$(parent).select2({
-			placeholder: "Выберите категорию",
-			data: data,
-		});
-		if($(parent).hasClass("update")) {
-			var id = $(parent).attr("data-id");
-			$(parent).select2("val", id);
-			$(parent).trigger("select2:select");
-		}
-	}, "json");
-	
+	updateParent(parent);
+
 	$(parent).on("select2:select", function(event) {
-		if($(category).length === 0) {
-			return;
-		}
-
-		var id = $(this).select2("val");
-
-		$(category).html('');
-		$(category).parents(".form-group").removeClass("has-error");
-		$(category).parents(".form-group").removeClass("has-success");
-
-		$.post("/admin/select/inherits", {"parent_id": id}, function(data) {
-			if(data) {
-				$(category).select2({
-					placeholder: "Выберите подкатегорию",
-					data: data,
-				}).select2("val", "");;
-
-				if($(category).hasClass("update")) {
-					var id = $(category).attr("data-id");
-					$(category).select2("val", id);
-					$(category).trigger("select2:select");
-				}
-			} else {
-				$(category).select2("val", "");
-			}
-		}, "json");
+		updateCategory(category, parent);
 	});
 
 });
