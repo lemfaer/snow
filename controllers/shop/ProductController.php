0167b1937c129912abdb84222e202d9827f2ca73
lem <lemfaer@gmail.com>
2016-05-01 22:14:58 +0300

    ProductController updater
    1) tabs fixed
    2) exception check added

--- controllers/shop/ProductController.php
+++ controllers/shop/ProductController.php
@@ -1,23 +1,25 @@
 <?php
 
-	class ProductController {
+class ProductController {
 
-		public function actionIndex($productID) {
-			$productItem = ProductItem::findFirst(array("id" => $productID));
-			try {
-				$recomendedList = Product::findAll(array("is_recomended" => '1'), "rand()", 4);
-			} catch(QueryEmptyResultException $e) {
-				$recomendedList = array();
-			}
+	const REC_COUNT = 4;
+	const REC_SORT  = "rand()";
 
-			// echo "ID: $productID";
-			// echo "<pre>";
-			// print_r($productItem);
-			// echo "<br>";
-			// print_r($recomendedList);
-			// echo "</pre>";
-
-			View::template("shop/product/index.php", compact("productItem", "recomendedList"));
+	public function actionIndex($id) {
+		try {
+			$productItem = ProductItem::findFirst(array("id" => $id));
+		} catch(RecordNotFoundException $e) {
+			throw new PageNotFoundException("product not found", $e);
 		}
 
+		try {
+			$recomendedList = Product::findAll(array("is_recomended" => '1'), 
+				self::REC_SORT, self::REC_COUNT);
+		} catch(RecordNotFoundException $e) {
+			$recomendedList = array();
+		}
+
+		View::template("shop/product/index.php", compact("productItem", "recomendedList"));
 	}
+
+}
