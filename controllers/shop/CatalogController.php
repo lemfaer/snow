a65679ec706bf799fa67d3ae56de6c57f7c50410
lem <lemfaer@gmail.com>
2016-05-01 22:14:21 +0300

    CatalogController updated
    1) tabs fixed
    2) exception check added

--- controllers/shop/CatalogController.php
+++ controllers/shop/CatalogController.php
@@ -1,30 +1,33 @@
 <?php
 
-	class CatalogController {
+class CatalogController {
 
-		const LIMIT_PRODUCT_LIST = 12;
-		const SORT_ORDER = "id DESC";
+	const LIMIT_PRODUCT_LIST = 12;
+	const SORT_ORDER = "id DESC";
 
-		public static function actionIndex($categoryID, $page = 1) {
-			$category = Category::findFirst(array("id" => $categoryID));
-			
-			$order = self::SORT_ORDER;
-			$limit = self::LIMIT_PRODUCT_LIST;
-			$offset = ($page - 1) * $limit;
-			$total = Product::findCount(array("category_id" => $categoryID));
+	public static function actionIndex($id, $page = 1) {
+		try {
+			$category = Category::findFirst(array("id" => $id));
+		} catch(RecordNotFoundException $e) {
+			throw new PageNotFoundException("category not found", $e);
+		}
+		
+		$order  = self::SORT_ORDER;
+		$limit  = self::LIMIT_PRODUCT_LIST;
+		$offset = ($page - 1) * $limit;
+		$total  = Product::findCount(array("category_id" => $id));
 
-			$productList = Product::findAll(array("category_id" => $categoryID), $order,
+		try {
+			$productList = Product::findAll(array("category_id" => $id), $order,
 				$limit, $offset);
-
-			$pagination = new Pagination($total, $page, $limit, "page-");
-
-			// echo "ID: $categoryID";
-			// echo "<pre>";
-			// print_r($productList);
-			// echo "</pre>";
-
-			View::template("shop/catalog/index.php", 
-				compact("category", "productList", "pagination"));
+		} catch(RecordNotFoundException $e) {
+			throw new PageNotFoundException("products not found", $e);
 		}
 
+		$pagination = new Pagination($total, $page, $limit, "page-");
+
+		View::template("shop/catalog/index.php", 
+			compact("category", "productList", "pagination"));
 	}
+
+}
