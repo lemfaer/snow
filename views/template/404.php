26f561208baa0309b391b9acc850364e61337a35
lem <lemfaer@gmail.com>
2016-05-01 22:22:03 +0300

    error pages added/updated

---
+++ views/template/404.php
@@ -0,0 +1,15 @@
+<div class="main">
+<div class="check">
+<div class="container">
+
+<div class="cart">
+	<h4 class="title">404 Страница не найдена</h4>
+	<p class="cart">
+		Мы не смогли найти страницу, которую вы искали. 
+		Вы можете <a href="/">вернуться на главную</a>.
+	</p>
+</div>
+
+</div>
+</div>
+</div>
--- views/template/error.php
+++ views/template/error.php
@@ -1,12 +1,14 @@
 <div class="main">
-	<div class="check">
-		<div class="container">
-			<div class="cart">
-				<h4 class="title">неизвестная ошибка</h4>
-				<p class="cart">
-					Нажмите<a onclick="location.href='/'"> тут</a> чтобы вернуться на главную
-				</p>
-			</div>
-		</div>
-	</div>
+<div class="check">
+<div class="container">
+
+<div class="cart">
+	<h4 class="title">неизвестная ошибка</h4>
+	<p class="cart">
+		Нажмите<a href="/"> тут</a> чтобы вернуться на главную
+	</p>
+</div>
+
+</div>
+</div>
 </div>
