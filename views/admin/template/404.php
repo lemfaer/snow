18440ea648f9cd3d922b22e0f1736fff50a2a4a7
lem <lemfaer@gmail.com>
2016-05-01 22:20:42 +0300

    admin error pages added

---
+++ views/admin/template/404.php
@@ -0,0 +1,18 @@
+<section class="content-header">
+  <h1>
+    404 Страница не найдена
+  </h1>
+</section>
+
+<section class="content">
+  <div class="error-page">
+    <h2 class="headline text-yellow"> 404</h2>
+    <div class="error-content">
+      <h3><i class="fa fa-warning text-yellow"></i> К сожалению страница не найдена.</h3>
+      <p>
+        Мы не смогли найти страницу, которую вы искали. 
+        Вы можете <a href="/admin">вернуться на главную</a>.
+      </p>
+    </div><!-- /.error-content -->
+  </div><!-- /.error-page -->
+</section><!-- /.content -->
---
+++ views/admin/template/error.php
@@ -0,0 +1,18 @@
+<section class="content-header">
+  <h1>
+    Неизвестная ошибка
+  </h1>
+</section>
+
+<section class="content">
+  <div class="error-page">
+    <h2 class="headline text-red">+</h2>
+    <div class="error-content">
+      <h3><i class="fa fa-warning text-red"></i> К сожалению что-то пошло не так.</h3>
+      <p>     
+        Мы уже работаем над исправлением ошибки. <br>
+        <a href="../../index.html">На главную</a>
+      </p>
+    </div>
+  </div><!-- /.error-page -->
+</section><!-- /.content -->
